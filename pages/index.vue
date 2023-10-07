9a89564a1f319836b465824c581e05033b699bb2
sayako21 <>
2023-10-08 04:11:36 +0900

    数パターンデザイン考える

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    <div class="main">
+      <div class="p-3">
+        <BingoCard4 />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index2.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gradient-to-b from-cyan-400 to-blue-500">
+    <div class="main">
+      <div class="p-10">
+        <BingoCard2 />
+      </div>
+    </div>
+  </div>
+</template>
