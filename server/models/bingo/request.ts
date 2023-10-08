3e3cfccf5ec930bac3d7e38e0c1fad89ac4938ff
Ichirou.S <sey323@users.noreply.github.com>
2023-10-08 23:50:43 +0900

    9 beアップロードapi (#13)

--- server/models/bingo/request.ts
+++ server/models/bingo/request.ts
@@ -1,1 +1,18 @@
 interface BongoCreateRequest {}
+
+interface BingoCellPostRequest {
+  bingoCellId: string;
+  comments: string;
+}
+
+/**
+ * ビンゴセルの更新リクエストに利用するDTO
+ */
+interface UpdateBingoCellDto {
+  imageUrl: string | null; // ビンゴの画像のURL
+  comments: string | null; // 投稿した画像に対するコメント(オプション)
+  answered_user: number | null; // 回答したユーザのID
+  answered_at: Date | null; // 回答した日時
+  geo_location: string | null; // 回答した場所の緯度経度
+  completed: boolean; // 完了したかどうか
+}
