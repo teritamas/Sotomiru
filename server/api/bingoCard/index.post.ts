fc6dc6a7fd5566552f70dee6868c220367640120
Ichirou.S <sey323@users.noreply.github.com>
2023-10-08 17:14:27 +0900

    2 beビンゴ画面取得api (#8)

---
+++ server/api/bingoCard/index.post.ts
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
+import { addBingoCard } from "@/server/facades/repositories/bingoContents";
+import { BingoCard, BingoContent } from "@/server/models/bingo/dto";
+
+/**
+ * ビンゴカードを新規作成する
+ */
+export default defineEventHandler(async (event) => {
+  try {
+    const body: BongoCreateRequest = await readBody(event);
+
+    const entryBingoCard = createBingoCard();
+
+    // DBに追加
+    addBingoCard(entryBingoCard);
+
+    return {
+      message: "OK",
+      bingoCardId: entryBingoCard.id,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      message: "Internal Server Error",
+    };
+  }
+});
+
+/**
+ * ビンゴカードを作成する
+ */
+function createBingoCard() {
+  const entryBingoCard = {
+    id: uuidv4(),
+    name: "test",
+    bingoCells: [],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  } as BingoCard;
+
+  sampleBingoCell.forEach((element) => {
+    entryBingoCard.bingoCells.push({
+      id: uuidv4(),
+      name: element.title,
+      description: element.description,
+      created_user: 0,
+      created_at: new Date(),
+      updated_at: new Date(),
+      completed: false,
+    } as BingoContent);
+  });
+  return entryBingoCard;
+}
+
+// サンプル用のお題
+// 将来的にChatGPTが自動生成するようにする
+const sampleBingoCell = [
+  {
+    title: "自然の美しさ",
+    description: "美しい山々、湖、森林などの自然の景色を捉えた写真",
+  },
+  {
+    title: "街の風景",
+    description: "ドライブ中に通過する街や町の風景をキャッチした写真",
+  },
+  {
+    title: "路の途中のカフェ",
+    description: "道路脇にあるカフェやレストランでの風景や料理を撮影した写真",
+  },
+  {
+    title: "休憩ポイント",
+    description:
+      "ドライブ中に立ち寄った休憩スポットでの風景や活動を記録した写真",
+  },
+  {
+    title: "季節の変化",
+    description: "季節ごとの変化を示す風景や植物を捉えた写真",
+  },
+  {
+    title: "地元のアート",
+    description: "地元のアート作品、壁画、彫刻などを探し、撮影した写真",
+  },
+  {
+    title: "車のディテール",
+    description: "愛車のディテールやカスタマイズをアップクローズで撮影した写真",
+  },
+  {
+    title: "日の出または日の入り",
+    description: "美しい日の出や日の入りをキャッチした写真",
+  },
+  {
+    title: "道路のカーブ",
+    description: "道路のカーブや山道の風景を楽しんで撮影した写真",
+  },
+];
---
+++ server/facades/repositories/bingoContents.ts
@@ -0,0 +1,34 @@
+import { firestore } from "@/server/firebase";
+import { BingoCard } from "@/server/models/bingo/dto";
+
+/**
+ * ビンゴカードの内容を取得する
+ */
+export const getBingoCard = async (bingoCardId: string) => {
+  try {
+    const querySnapshot = await firestore
+      .collection("bingoCard")
+      .doc(bingoCardId)
+      .get();
+    const bingoCard = querySnapshot.data() as BingoCard;
+    console.log(bingoCardId, querySnapshot.data());
+    return bingoCard;
+  } catch (e) {
+    console.error("[getBingoCard]", e);
+  }
+};
+
+/**
+ * ビンゴカードを新規作成する
+ */
+export const addBingoCard = async (bingoCard: BingoCard) => {
+  try {
+    const docRef = await firestore
+      .collection("bingoCard")
+      .doc(bingoCard.id)
+      .set(bingoCard);
+    return bingoCard.id;
+  } catch (e) {
+    console.error("[addBingoCard]", e);
+  }
+};
