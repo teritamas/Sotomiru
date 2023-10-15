f6d4edb854a2f28757bab86e7d54fb413cd48054
sey323 <sey323_nm@yahoo.co.jp>
2023-10-15 22:31:40 +0900

    ビンゴカードのポップアップが表示されない不具合

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="main">
-    <BingoCardCarousel :bingoCards="bingoCards" />
+    <BingoCardCarousel
+      @postBingoCellRequest="postBingoCellRequest"
+      @postCheckFollowingSubject="postCheckFollowingSubject"
+      :bingoCards="bingoCards"
+      :isFollowingSubject="isFollowingSubject"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { BingoCard } from "@/server/models/bingo/dto";
-import { BingoCardsGetAllResponse } from "@/server/models/bingo/response";
+import {
+  BingoCardsGetAllResponse,
+  BingoCardsGetResponse,
+} from "@/server/models/bingo/response";
+import { IsFollowingSubjectResponse } from "~/server/models/facades/visionai/imageDescription";
 
 const bingoCards = ref([] as BingoCard[]);
+const bingoCardId = ref("");
+const isFollowingSubject = ref(null as IsFollowingSubjectResponse | null);
 
 // 全てのビンゴカードを取得
 onMounted(async () => {
@@ -20,6 +31,62 @@ onMounted(async () => {
 const getAllBingoCard = async () => {
   const res = await fetch(`api/bingoCard`);
   const data = (await res.json()) as BingoCardsGetAllResponse;
+  return data;
+};
+
+// アップロードした画像がテーマに沿っているかを確認する。
+const postCheckFollowingSubject = async (bingoCellId: string, file: any) => {
+  // 前の検証結果をクリア
+  isFollowingSubject.value = null;
+
+  // リクエストボディの作成
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append(
+    "request",
+    JSON.stringify({
+      bingoCellId: bingoCellId,
+    } as CheckFollowingSubjectPostRequest)
+  );
+  const res = await fetch(
+    `/api/bingoCell/checkFollowingSubject/${bingoCardId}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  isFollowingSubject.value = await res.json();
+};
+
+// ビンゴカードの詳細を投稿する
+// 投稿完了後、ビンゴカードの最新の状態を取得し、モーダルを閉じる
+const postBingoCellRequest = async (
+  bingoCellId: string,
+  form: { comments: string },
+  file: any
+) => {
+  const formData = new FormData();
+  formData.append(
+    "request",
+    JSON.stringify({
+      bingoCellId: bingoCellId,
+      ...form,
+    } as BingoCellPostRequest)
+  );
+  formData.append("file", file);
+  await useFetch(`/api/bingoCell/${bingoCardId}`, {
+    method: "PUT",
+    body: formData,
+  });
+  const res = await fetchBingoCard();
+  // bingoCard.value = res.bingoCard;
+  // modalIsOpen.value = false;
+};
+
+// ビンゴカードの情報取得
+const fetchBingoCard = async () => {
+  const res = await fetch(`/api/bingoCard/${bingoCardId}`);
+  const data = (await res.json()) as BingoCardsGetResponse;
   return data;
 };
 </script>
