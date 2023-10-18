10f5888c59987ab250690744a5741eb30abc6ca2
sayako21 <>
2023-10-18 22:29:42 +0900

    [追加]ヘッダー追加

--- pages/create.vue
+++ pages/create.vue
@@ -87,13 +87,21 @@
           </div>
         </fieldset>
       </div>
-      <button
-        @click="createBingoCard()"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        :disabled="submitting"
-      >
-        {{ submitButtonMessage }}
-      </button>
+      <div class="flex justify-between">
+        <router-link
+          class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+          to="/"
+        >
+          戻る
+        </router-link>
+        <button
+          @click="createBingoCard()"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          :disabled="submitting"
+        >
+          {{ submitButtonMessage }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
