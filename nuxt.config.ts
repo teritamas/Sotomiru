b23d37245b7523142ae7e0fc35d0b3d06e58e34b
sey323 <sey323_nm@yahoo.co.jp>
2023-10-24 23:33:15 +0900

    認証情報の設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire"],
   nitro: {
     preset: "firebase",
     firebase: {
       gen: 2,
+    },
+  },
+  vuefire: {
+    auth: true, // enable Firebase Authentication
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
     },
   },
 });
