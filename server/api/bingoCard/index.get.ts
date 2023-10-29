b75ae3ab186fe17d4fedeab4d1a162e770e5bf2f
Ichiro.S <sey323@users.noreply.github.com>
2023-10-30 01:35:22 +0900

    :bug: ビンゴ/リーチ判定 (#63)

--- server/api/bingoCard/index.get.ts
+++ server/api/bingoCard/index.get.ts
@@ -4,7 +4,12 @@ import {
 } from "@/server/facades/repositories/bingoContents";
 import { BingoCardsGetAllResponse } from "@/server/models/bingo/response";
 import { idAuthentication } from "@/server/facades/auth/idAuthentication";
-import { BingoCard } from "@/server/models/bingo/dto";
+import {
+  BingoCard,
+  BingoCardDetail,
+  CompleteBingoLine,
+  ReachLine,
+} from "@/server/models/bingo/dto";
 
 /**
  * ビンゴカードを　全件取得する
@@ -23,8 +28,17 @@ export default defineEventHandler(async (event) => {
         bingoCards = await getAllBingoCard();
       });
 
+    const bingoCardDetails = bingoCards.map((bingoCard) => {
+      const checkBingoOrReachLinesResult = checkBingoOrReachLines(bingoCard);
+      return {
+        ...bingoCard,
+        completeBingoLines: checkBingoOrReachLinesResult.completeBingoLines,
+        reachLines: checkBingoOrReachLinesResult.reachLines,
+      } as BingoCardDetail;
+    });
+
     return {
-      bingoCards: bingoCards,
+      bingoCardDetails: bingoCardDetails,
     } as BingoCardsGetAllResponse;
   } catch (e) {
     console.error(e);
@@ -33,3 +47,84 @@ export default defineEventHandler(async (event) => {
     };
   }
 });
+
+/**
+ * ビンゴかリーチが成立していることを確認する。
+ */
+function checkBingoOrReachLines(bingoCard: BingoCard) {
+  const bingoCardCells = bingoCard.bingoCells;
+  const completeBingoLines = [];
+  const reachLines = [];
+
+  // 行のチェック
+  for (let i = 0; i < 3; i++) {
+    const row = bingoCardCells.slice(i * 3, i * 3 + 3);
+    if (row.every((cell) => cell.completed)) {
+      completeBingoLines.push({
+        row: i,
+        column: null,
+      } as CompleteBingoLine);
+    }
+    // 一つだけ空白の場合は、リーチ
+    if (row.filter((cell) => cell.completed).length === 2) {
+      reachLines.push({
+        row: i,
+        column: null,
+      } as ReachLine);
+    }
+  }
+
+  // 列のチェック
+  for (let i = 0; i < 3; i++) {
+    const column = [
+      bingoCardCells[i],
+      bingoCardCells[i + 3],
+      bingoCardCells[i + 6],
+    ];
+    if (column.every((cell) => cell.completed)) {
+      completeBingoLines.push({
+        row: null,
+        column: i,
+      } as CompleteBingoLine);
+    }
+    // 一つだけ空白の場合は、リーチ
+    if (column.filter((cell) => cell.completed).length === 2) {
+      reachLines.push({
+        row: null,
+        column: i,
+      } as ReachLine);
+    }
+  }
+
+  // 斜めのチェック
+  const diagonal1 = [bingoCardCells[0], bingoCardCells[4], bingoCardCells[8]]; // 左上から右下
+  if (diagonal1.every((cell) => cell.completed)) {
+    completeBingoLines.push({
+      row: 0,
+      column: 0,
+    } as CompleteBingoLine);
+  }
+  // 一つだけ空白の場合は、リーチ
+  if (diagonal1.filter((cell) => cell.completed).length === 2) {
+    reachLines.push({
+      row: 0,
+      column: 0,
+    } as ReachLine);
+  }
+  const diagonal2 = [bingoCardCells[2], bingoCardCells[4], bingoCardCells[6]]; // 右上から左下
+  if (diagonal2.every((cell) => cell.completed)) {
+    completeBingoLines.push({
+      row: 2, // 右
+      column: 2, // 上
+    } as CompleteBingoLine);
+  }
+  // 一つだけ空白の場合は、リーチ
+  if (diagonal2.filter((cell) => cell.completed).length === 2) {
+    reachLines.push({
+      row: 2,
+      column: 2,
+    } as ReachLine);
+  }
+
+  return { completeBingoLines, reachLines };
+}
