3c89572f8d923e5502dd1e22490a6a66062b6230
sey323 <sey323_nm@yahoo.co.jp>
2023-11-13 21:11:43 +0900

    :+1: データをリクエストボディから取得する

--- functions/contract_proxy/contract_proxy/config.py
+++ functions/contract_proxy/contract_proxy/config.py
@@ -3,11 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-# FireStoreの認証キー
-CRED_PATH = environ.get(
-    "CRED_PATH", "../../key.json"
-)  # functions/create_complete_movieからのパス
-
 THIRDWEB_API_KEY = environ.get("THIRDWEB_API_KEY", "")
 THIRDWEB_SIGNER_PRIVATEKEY = environ.get("THIRDWEB_SIGNER_PRIVATEKEY", "")
 ERC1155_CONTRACT_ADDRESS = environ.get("ERC1155_CONTRACT_ADDRESS", "")
