e98209ef0fca8967bca05414cc7c46ba1145a687
sey323 <sey323_nm@yahoo.co.jp>
2023-11-13 20:51:42 +0900

    :bug: 回答前の項目をoptionalに変更

--- functions/contract_proxy/contract_proxy/models/bingo_card.py
+++ functions/contract_proxy/contract_proxy/models/bingo_card.py
@@ -4,17 +4,19 @@ from pydantic import BaseModel, Field
 
 
 class BingoCardCell(BaseModel):
-    answered_at: Optional[datetime] = Field(None, description="回答日時")
-    answered_user: Optional[str] = Field(None, description="回答者のID")
     id: str = Field(..., description="ID")
-    comments: str = Field("", description="コメント")
     completed: bool = Field(..., description="回答済みかどうか")
     created_at: datetime = Field(..., description="作成日時")
     # created_user: str = Field(..., description="作成者のID")
     description: str = Field(..., description="説明")
-    imageUrl: Optional[str] = Field(None, description="画像のURL")
     name: str = Field(..., description="名前")
-    imageAiCheckScore: int = Field(None, description="画像AIチェックのスコア")
+
+    # 投稿後に追加される項目
+    comments: Optional[str] = Field("", description="コメント")
+    imageUrl: Optional[str] = Field(None, description="画像のURL")
+    imageAiCheckScore: Optional[int] = Field(None, description="画像AIチェックのスコア")
+    answered_at: Optional[datetime] = Field(None, description="回答日時")
+    answered_user: Optional[str] = Field(None, description="回答者のID")
 
 
 class BingoUserTokenMintDto(BaseModel):
